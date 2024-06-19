04935e69e6a4cfb9ff8f709fe90a4d20c0435809
97250 <adi.Cohen37@mail.huji.ac.il>
2024-06-19 20:38:58 +0300

    added test_wallet_attack_adi

--- ex4_files/part1/test_part1.py
+++ ex4_files/part1/test_part1.py
@@ -2,13 +2,14 @@ from solcx import compile_files, install_solc
 from web3 import Web3
 import solcx
 
-SOLC_VERSION = 'v0.8.19'
+SOLC_VERSION = '0.8.19'
 
 # Ensure you have the appropriate Solidity compiler version installed
 install_solc(SOLC_VERSION)
 
-get_balance = lambda address: w3.from_wei(w3.eth.get_balance(address), 'ether')
-
+# Helper function to get balance in ether
+def get_balance(address):
+    return w3.from_wei(w3.eth.get_balance(address), 'ether')
 
 # Compile Solidity source code
 def compile(file_name: str):
@@ -19,15 +20,13 @@ def compile(file_name: str):
         contract_id, contract_interface = compiled_sol.popitem()
     return contract_interface['bin'], contract_interface['abi']
 
-
 # Compile the contracts
-wallet_bytecode, wallet_abi = compile(r'../part1/VulnerableWallet.sol')
-attack_bytecode, attack_abi = compile(r'../part1/WalletAttack.sol')
+wallet_bytecode, wallet_abi = compile(r'C:\Users\97250\Documents\exricises\Cryptocurrencies\git_exercise4\ex4_files\part1\VulnerableWallet.sol')
+attack_bytecode, attack_abi = compile(r'C:\Users\97250\Documents\exricises\Cryptocurrencies\git_exercise4\ex4_files\part1\WalletAttack.sol')
 
 # Web3 connection
 w3 = Web3(Web3.HTTPProvider("http://127.0.0.1:8545"))
-
-
+accounts = w3.eth.accounts
 def test_wallet_attack():
     # Deploy VulnerableWallet contract
     vulnerable_wallet_interface = w3.eth.contract(abi=wallet_abi, bytecode=wallet_bytecode)
@@ -62,5 +61,47 @@ def test_wallet_attack():
     print(get_balance(w3.eth.accounts[0]))
     print(get_balance(w3.eth.accounts[1]))
 
-# Run the test
-test_wallet_attack()
+def test_wallet_attack_adi():
+    # Deploy VulnerableWallet contract
+    vulnerable_wallet_interface = w3.eth.contract(abi=wallet_abi, bytecode=wallet_bytecode)
+    vulnerable_tx_hash = vulnerable_wallet_interface.constructor().transact({'from': accounts[1]})
+    vulnerable_tx_receipt = w3.eth.wait_for_transaction_receipt(vulnerable_tx_hash)
+    vulnerable_wallet_address = vulnerable_tx_receipt.contractAddress
+    vul_wallet_instance = w3.eth.contract(address=vulnerable_wallet_address, abi=wallet_abi)
+
+    # Deploy WalletAttack contract
+    wallet_attack_interface = w3.eth.contract(abi=attack_abi, bytecode=attack_bytecode)
+    wallet_attack_tx_hash = wallet_attack_interface.constructor().transact({'from': accounts[2]})
+    wallet_attack_tx_receipt = w3.eth.wait_for_transaction_receipt(wallet_attack_tx_hash)
+    wallet_attack_address = wallet_attack_tx_receipt.contractAddress
+    wallet_attack_instance = w3.eth.contract(address=wallet_attack_address, abi=attack_abi)
+
+    # Initial balances
+    print(f"Initial attacker balance: {get_balance(wallet_attack_address)} ETH")
+    print(f"Initial vulnerable wallet balance: {get_balance(vulnerable_wallet_address)} ETH")
+
+    # Deposit 3 Ether to VulnerableWallet
+    tx_hash = vul_wallet_instance.functions.deposit().transact({'from': accounts[0], 'value': w3.to_wei(3, 'ether')})
+    w3.eth.wait_for_transaction_receipt(tx_hash)
+    print(f"Balance of vulnerable wallet after deposit: {get_balance(vulnerable_wallet_address)} ETH")
+    assert get_balance(vulnerable_wallet_address) == 3
+
+    # Call exploit function in WalletAttack contract
+    wallet_attack_instance.functions.exploit(vulnerable_wallet_address).transact({'from': accounts[1], 'value': w3.to_wei(1, 'ether')})
+    w3.eth.wait_for_transaction_receipt(tx_hash)
+    print("Attack executed successfully")
+    # Check the balance of VulnerableWallet after the attack
+    balance_after_attack = get_balance(vulnerable_wallet_address)
+    print(f"Balance of vulnerable wallet after attack: {balance_after_attack} ETH")
+    assert balance_after_attack == 0
+
+    # Check the balance of attacker after the attack
+    attacker_balance_after_attack = get_balance(wallet_attack_address)
+    print(f"Balance of attacker wallet after attack: {attacker_balance_after_attack} ETH")
+    assert attacker_balance_after_attack == 3
+
+    # Check the balance of accounts[2] after the attack
+    account1_balance_after_attack = get_balance(accounts[2])
+    print(f"Balance of account[2]/attacker after attack: {account1_balance_after_attack} ETH")
+
+
